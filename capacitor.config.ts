300d0197d7960e526e8befc0fb486cb4e431c57d
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-02-21 19:13:30 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tca-foo-react-ionic',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
