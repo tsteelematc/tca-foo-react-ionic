2bf18a7f34774ff560916421efd39f329155409f
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2022-02-28 18:56:12 -0600

    Got navigation working...

--- src/App.tsx
+++ src/App.tsx
@@ -21,8 +21,8 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import { SetupGame } from './pages/SetupGame';
-import { PlayGame } from './pages/PlayGame';
+import SetupGame from './pages/SetupGame';
+import PlayGame from './pages/PlayGame';
 
 setupIonicReact();
 
